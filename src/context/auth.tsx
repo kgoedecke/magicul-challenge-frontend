4ea9e30eb84014146ccb363edd7568cd5452432b
Philipe <philipep.galdino@gmail.com>
2021-12-08 23:29:15 -0300

    feat: Added both Files and Auth contexts & providers

---
+++ src/context/auth.tsx
@@ -0,0 +1,72 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+import Router from 'next/router'
+import { destroyCookie, setCookie } from 'nookies'
+
+import { api, Endpoints } from '@/services/api/index'
+import { useFiles } from './files'
+
+export type User = {
+  id: number
+  name: string
+}
+
+type LoginProps = {
+  name: string
+}
+
+type IAuthContextData = {
+  user: User | undefined
+  isLoggedIn: boolean
+  logIn(name: LoginProps): void
+}
+
+interface AuthProviderProps {
+  children?: ReactNode
+}
+
+const AuthContext = createContext({} as IAuthContextData)
+
+export function logOut() {
+  destroyCookie(null, `auth.token`)
+  destroyCookie(null, `auth.refreshToken`)
+
+  Router.push(`/`)
+}
+
+const AuthProvider: React.FC = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User>()
+  const isLoggedIn = !!user
+
+  async function logIn({ name }: LoginProps) {
+    try {
+      // eslint-disable-next-line no-console
+      console.log(`Logging in ${name}`)
+      const response = await api.post(Endpoints.LOGIN, {
+        name,
+      })
+
+      const { id } = response.data
+
+      setUser({ name, id })
+
+      Router.push(`/upload`)
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err)
+    }
+  }
+
+  return <AuthContext.Provider value={{ logIn, isLoggedIn, user }}>{children}</AuthContext.Provider>
+}
+
+function useAuth(): IAuthContextData {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error(`useAuth must be used within AuthProvider`)
+  }
+
+  return context
+}
+
+export { AuthProvider, useAuth }
---
+++ src/context/files.tsx
@@ -0,0 +1,145 @@
+/* eslint-disable no-console */
+import React, { createContext, useState, useCallback, useContext } from 'react'
+import filesize from 'filesize'
+
+import { api, Endpoints } from '@/services/api/index'
+import { useAuth } from './auth'
+
+export interface IFile {
+  userId: number
+  id?: number
+  filename: string
+  mimeType: string
+  size: string
+  uploaded: boolean
+  error: boolean
+  progress?: number
+  file: File
+}
+
+interface IFileContextData {
+  uploadedFiles: IFile[]
+  deleteFile(id: number): void
+  handleUpload(file: any, userId: number): void
+  loadUserFiles(userId: number): void
+}
+
+const FileContext = createContext<IFileContextData>({} as IFileContextData)
+
+const FileProvider: React.FC = ({ children }) => {
+  const [uploadedFiles, setUploadedFiles] = useState<IFile[]>([])
+  const { user } = useAuth()
+
+  const loadUserFiles = useCallback((userId: number) => {
+    api
+      .get<IFile[]>(Endpoints.GET_FILES, {
+        params: {
+          user_id: userId,
+        },
+      })
+      .then((response) => {
+        const fileFormatted: IFile[] = response.data.map((file) => ({
+          ...file,
+          id: file.id,
+          size: file.size,
+          filename: file.filename,
+          mimeType: file.mimeType,
+          error: false,
+          uploaded: true,
+        }))
+
+        console.log(response.data)
+
+        setUploadedFiles(fileFormatted)
+      })
+  }, [])
+
+  const updateFile = useCallback((id, data) => {
+    setUploadedFiles((state) => state.map((file) => (file.id === id ? { ...file, ...data } : file)))
+  }, [])
+
+  const processUpload = useCallback(
+    (uploadedFile: IFile) => {
+      const data = new FormData()
+      if (uploadedFile.filename) {
+        data.append(`file`, uploadedFile.file)
+        data.append(`user_id`, JSON.stringify(user && user.id))
+      }
+
+      api
+        .post(Endpoints.UPLOAD_FILE, data, {
+          onUploadProgress: (progressEvent) => {
+            const progress: number = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+
+            console.log(`The file ${uploadedFile.filename} is ${progress}% processed... `)
+
+            updateFile(uploadedFile.id, { progress })
+          },
+        })
+        .then((response) => {
+          console.log(`The file ${uploadedFile.filename} was succesfully uploaded!`)
+
+          updateFile(uploadedFile.id, {
+            uploaded: true,
+            id: response.data.id,
+            name: response.data.name,
+          })
+        })
+        .catch((err) => {
+          console.error(`There was a problem with uploading the file ${uploadedFile.filename}`)
+          console.log(err)
+
+          updateFile(uploadedFile.id, {
+            error: true,
+          })
+        })
+    },
+    [updateFile, user],
+  )
+
+  const handleUpload = useCallback(
+    (files: File[], userId: number) => {
+      const newUploadedFiles: IFile[] = files.map((file: File) => ({
+        file,
+        userId,
+        filename: file.name,
+        size: filesize(file.size),
+        mimeType: file.type,
+        progress: 0,
+        uploaded: false,
+        error: false,
+      }))
+
+      setUploadedFiles((state) => state.concat(newUploadedFiles))
+      newUploadedFiles.forEach(processUpload)
+    },
+    [processUpload],
+  )
+
+  const deleteFile = useCallback((id: number) => {
+    api.delete(Endpoints.DELETE_FILE, {
+      params: {
+        fileId: id,
+      },
+    })
+    setUploadedFiles((state) => state.filter((file) => file.id !== id))
+  }, [])
+
+  return (
+    <FileContext.Provider value={{ uploadedFiles, deleteFile, handleUpload, loadUserFiles }}>
+      {children}
+    </FileContext.Provider>
+  )
+}
+
+function useFiles(): IFileContextData {
+  const context = useContext(FileContext)
+
+  if (!context) {
+    console.log(`useFiles must be used within FileProvider`)
+  }
+
+  return context
+}
+
+export { FileProvider, useFiles }
